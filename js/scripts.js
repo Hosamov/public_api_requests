031d0baa933b2c5ac38af0be0a82d3e38b9072b5
Hosamov <hosamov@hotmail.com>
2021-01-30 15:39:54 -0800

    add proper formatting for cell number and birthday

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 const randUserUrl = 'https://randomuser.me/api/?nat=US&results=12';
 const gallery = document.querySelector('.gallery');
 
+
+//----------------------------------FETCH DATA--------------------------------//
 //async function to return a promise resolved by parsing the body text as JSON
 async function getJSON(url) {
   try {
@@ -11,91 +13,109 @@ async function getJSON(url) {
   }
 }
 
+//-------------------------------HELPER FUNCTIONS-----------------------------//
+
+//Convert the date to the proper format(MM/DD/YYYY)
+function dateConverter(date) {
+  const newDate = new Date(date);
+  return new Intl.DateTimeFormat('en-US').format(newDate);
+}
+
+//Convert the cell number into the proper format (xxx) xxx-xxxx
+//Credit: https://stackoverflow.com/questions/8358084/regular-expression-to-reformat-a-us-phone-number-in-javascript
+function phoneNumConverter(num) {
+  let nonDigits = (num).replace(/\D/g, ''); //target, replace non-digit characters so we are left with numbers only
+  let number = nonDigits.match(/^(\d{3})(\d{3})(\d{4})$/); //organize/"match" numbers into three groups (3,3,4)
+  if(number) {
+    return `(${number[1]}) ${number[2]}-${number[3]}`; //correctly format the phone number '(xxx) xxx-xxxx'
+  }
+}
+
 //async function to gather user data required to be displayed from api
 async function getUserList(url) {
-    try {
-      const randomUserJSON = await getJSON(url); //get a Promise by calling getJSON()
-      //map out data from randomUserJSON in order to use the information
-      const profiles = randomUserJSON.results.map(person => {
-        const image = person.picture.large;
-        const fullName = `${person.name.first} ${person.name.last}`;
-        const email = person.email;
-        const loc = `${person.location.city}, ${person.location.state}`;
-        const cellNumber = person.cell;
-        const address = `${person.location.street.number} ${person.location.street.name}, ${loc} ${person.location.postcode}`;
-        const birthday = parseInt(person.dob.age);
+  try {
+    const randomUserJSON = await getJSON(url); //get a Promise by calling getJSON()
+    //map out data from randomUserJSON in order to use the information
+    const profiles = randomUserJSON.results.map(person => {
+      const image = person.picture.large;
+      const fullName = `${person.name.first} ${person.name.last}`;
+      const email = person.email;
+      const loc = `${person.location.city}, ${person.location.state}`;
+      const cellNumber = phoneNumConverter(person.cell); //Convert to proper format: (xxx) xxx-xxxx
+      const address = `${person.location.street.number} ${person.location.street.name}, ${loc} ${person.location.postcode}`;
+      const birthday = dateConverter(person.dob.date); //Convert to format: MM/DD/YYYY
 
-        //return all collected data var names for future use (see generateHTML())
-        return { image, fullName, email, loc, cellNumber, address };
-      });
-      return Promise.all(profiles); //return profiles variable
-    } catch (error) {
-      gallery.innerHTML = `<h1>An error occurred while fetching data.</h1>
+      //return all collected data var names for future use (see generateGalleryHTML())
+      return { image, fullName, email, loc, cellNumber, address, birthday };
+    });
+    return Promise.all(profiles); //return profiles variable
+  } catch (error) {
+    gallery.innerHTML = `<h1>An error occurred while fetching data.</h1>
       <h2>Please try again in a moment. If the error persists, please contact the webmaster.</h2>
       `
-    }
   }
-  //TODO: add array to store mapped vars
-  const personData = [];
+}
 
-  //Function to generate HTML data to div element with id of 'gallery'
-  function generateGalleryHTML(data) {
-    data.map(person => {
-      personData.push(person);
-      //Dynamically insert required data for 12 random user cards
-      gallery.insertAdjacentHTML('beforeend', `
+//decare an array to store the returned random user data
+const personData = [];
+
+//Function to generate HTML data to div element with id of 'gallery'
+function generateGalleryHTML(data) {
+  data.map(person => {
+    personData.push(person);
+  });
+
+  //Dynamically insert required data for 12 random user cards
+  for (let i = 0; i < personData.length; i++) {
+    gallery.insertAdjacentHTML('beforeend', `
       <div class="card">
           <div class="card-img-container">
-              <img class="card-img" src="${person.image}" alt="profile picture">
+              <img class="card-img" src="${personData[i].image}" alt="profile picture">
           </div>
           <div class="card-info-container">
-              <h3 id="name" class="card-name cap">${person.fullName}</h3>
-              <p class="card-text">${person.email}</p>
-              <p class="card-text cap">${person.loc}</p>
+              <h3 id="name" class="card-name cap">${personData[i].fullName}</h3>
+              <p class="card-text">${personData[i].email}</p>
+              <p class="card-text cap">${personData[i].loc}</p>
           </div>
       </div>
       `);
+  }
+
+  //target the newly-created children (cards) of gallery class
+  const cards = gallery.children;
+  //loop through list of cards
+  for (let i = 0; i < cards.length; i++) {
+    const card = cards[i]; //declare var for applicable card in order to 'click'
+    //event listener for the selected card:
+    card.addEventListener('click', () => {
+      //Dynamically insert the required information for selected modal object
+      gallery.insertAdjacentHTML('beforeend', `
+        <div class="modal-container">
+            <div class="modal">
+                <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                <div class="modal-info-container">
+                    <img class="modal-img" src="${personData[i].image}" alt="profile picture">
+                    <h3 id="name" class="modal-name cap">name</h3>
+                    <p class="modal-text">${personData[i].email}</p>
+                    <p class="modal-text cap">${personData[i].loc}</p>
+                    <hr>
+                    <p class="modal-text">${personData[i].cellNumber}</p>
+                    <p class="modal-text">${personData[i].address}</p>
+                    <p class="modal-text">Birthday: ${personData[i].birthday}</p>
+                </div>
+            </div>
+        `);
+
+      //Close Modal window button
+      const modalCloseBtn = document.getElementById('modal-close-btn');
+      modalCloseBtn.addEventListener('click', (e) => {
+        gallery.lastElementChild.remove();
+      });
     });
 
+  }
+}
 
-
-      const cards = gallery.children;
-      for(let i = 0; i < cards.length; i++) {
-        const card = cards[i];
-        card.addEventListener('click', () => {
-          console.log('card ' + i + ' was clicked');
-          //Dynamically insert information for selected modal object
-          gallery.insertAdjacentHTML('beforeend', `
-          <div class="modal-container">
-              <div class="modal">
-                  <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-                  <div class="modal-info-container">
-                      <img class="modal-img" src="${personData[i].image}" alt="profile picture">
-                      <h3 id="name" class="modal-name cap">name</h3>
-                      <p class="modal-text">${personData[i].email}</p>
-                      <p class="modal-text cap">${personData[i].loc}</p>
-                      <hr>
-                      <p class="modal-text">${personData[i].cellNumber}</p>
-                      <p class="modal-text">${personData[i].address}</p>
-                      <p class="modal-text">Birthday: ${personData[i].birthday}</p>
-                  </div>
-              </div>
-          `);
-          const modalContainer = document.querySelector('#modal-container');
-          const modalCloseBtn = document.getElementById('modal-close-btn');
-          modalCloseBtn.addEventListener('click', (e) => {
-            console.log('closed');
-            gallery.lastElementChild.remove();
-          });
-        });
-
-      }
-    }
-
-    //Function to generate Modal HTML data to div element with id of 'gallery'
-    function generateModalHTML(data) {
-
-    }
-
-    getUserList(randUserUrl) //1. get the user postData
-      .then(generateGalleryHTML) //2. Pass the data to generateHTML()
+//--------------------------------CALL FUNCTIONS------------------------------//
+getUserList(randUserUrl) //1. get the user postData
+  .then(generateGalleryHTML) //2. Pass the data to generateHTML()
